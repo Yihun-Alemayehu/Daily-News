95d96fdbc5aabda3761c65e3b07e0ea0b828afd8
Yihun <yankure01@gmail.com>
2024-02-20 22:51:34 +0300

    fdb-01

---
+++ lib/features/daily_news/presentation/bloc/news_event.dart
@@ -0,0 +1,8 @@
+part of 'news_bloc.dart';
+
+sealed class NewsEvent extends Equatable {
+  const NewsEvent();
+
+  @override
+  List<Object> get props => [];
+}
---
+++ lib/features/daily_news/presentation/bloc/news_state.dart
@@ -0,0 +1,10 @@
+part of 'news_bloc.dart';
+
+sealed class NewsState extends Equatable {
+  const NewsState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+final class NewsInitial extends NewsState {}
